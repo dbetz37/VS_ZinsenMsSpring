7f6b8e4d7dd2d1c3f6822a3bd75d7c5ae42396a5
betz <dbetz@web.de>
2021-03-03 15:20:52 +0100

    Version 2.0

--- src/main/java/com/example/demo/BauController.java
+++ src/main/java/com/example/demo/BauController.java
@@ -1,21 +1,38 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/baukapital")
 public class BauController {
-    public int berechneLaufzeitInMonaten(double kreditbetrag, double zinssatz, double rueckzahlung){
+    @GetMapping
+    public int berechneGet(
+            @RequestParam double kreditbetrag,
+            @RequestParam double zinssatz,
+            @RequestParam double rueckzahlung) {
+        return berechneLaufzeitInMonaten(kreditbetrag, zinssatz, rueckzahlung);
+    }
+    @PostMapping
+    public int berechnePost(@RequestBody Map<String, String> parameter) {
+        double kreditbetrag = Double.parseDouble(parameter.get("kreditbetrag"));
+        double zinssatz = Double.parseDouble(parameter.get("zinssatz"));
+        double rueckzahlung = Double.parseDouble(parameter.get("ruekzahlung"));
+        return berechneLaufzeitInMonaten(kreditbetrag, zinssatz, rueckzahlung);
+    }
+    int berechneLaufzeitInMonaten(double kreditbetrag, double zinssatz, double rueckzahlung){
         double restBetrag = kreditbetrag;
         int monate = 0;
+        double zinsen;
+        double tilgung;
+
         while(restBetrag > 0){
-            double zinsen = restBetrag*zinssatz;
-            double tilgung = rueckzahlung - zinsen;
+            zinsen = restBetrag*zinssatz;
+            tilgung = rueckzahlung - zinsen;
             if(restBetrag-tilgung > 0){
-                restBetrag = restBetrag - tilgung;
+                restBetrag -= tilgung;
             } else {
                 restBetrag = 0;
             }
